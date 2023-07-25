186f3df0672babd72cadccae9eb80b995d0ac5da
Mohammed Murshid <105794198+mmurshidf@users.noreply.github.com>
2023-07-25 17:45:39 -0400

    Connected to DB
    Connected to DataBase

--- server/index.js
+++ server/index.js
@@ -7,28 +7,37 @@ import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
-import { fileUrlToPath } from "url";
+import { fileURLToPath } from "url";
 
-const __filename = fileUrlToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json({ limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "public/assets");
+      cb(null, "public/assets");
     },
     filename: function (req, file, cb) {
-        cb(null, fule.originalname);
-    }
+      cb(null, file.originalname);
+    },
 });
 const upload = multer({ storage });
+
+
+const PORT = 5174 || 6001;
+mongoose.connect("mongodb+srv://Faizan:nQMmTbhr9L8eM1VL@cluster0.jaadlpc.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log('Server Port: ${PORT}'));
+}).catch((error) => console.log('${error} did not connect'));
